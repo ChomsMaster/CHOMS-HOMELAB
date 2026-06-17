a04044bfbb0d9094fd02d0901ca9642bb4400f14
Oscar Salcedo <oscar_salcedo@hotmail.com>
2026-06-17 08:34:35 +0200

    Add Docker installation script

---
+++ scripts/03-install-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+echo "Installing Docker..."
+
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/debian/gpg \
+| sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable --now docker
+
+sudo usermod -aG docker "$USER"
+
+echo "Docker installed."
+echo "Log out and log back in for Docker permissions to apply."
